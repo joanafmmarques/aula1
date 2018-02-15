31a841d0876593a2cde8b8df098fe66508f6483c
Joana Marques <joana.pipaa@gmail.com>
2018-02-15 16:11:46 +0000

    Modificar codigo- ler argumentos

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -6,8 +6,9 @@ namespace OlaMundo
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Ola Mundo!");
-            Console.ReadKey();
+            for (int i = 0; i < args.Length; i++){
+                Console.WriteLine(args[i]);
+            }
         }
     }
 }
